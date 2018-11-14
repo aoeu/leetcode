602a5a9a7e13eb3724c0cc48fc631d983173cc60
Travis McDemus <travis@mcdem.us>
2018-11-14 15:17:53 -0500

    Add test case for problem 804

--- 0804-unique-morse-code-words_test.go
+++ 0804-unique-morse-code-words_test.go
@@ -3,5 +3,15 @@ package leetcode
 import "testing"
 
 func TestUniqueMorseRepresentations(t *testing.T) {
-
+	testCases := []struct {
+		in  []string
+		out int
+	}{
+		{[]string{"gin", "zen", "gig", "msg"}, 2},
+	}
+	for _, tc := range testCases {
+		if n := uniqueMorseRepresentations(tc.in); n != tc.out {
+			t.Errorf("Expected a count of %v but received %v for input %v", tc.out, n, tc.in)
+		}
+	}
 }
