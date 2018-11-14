d7f3e3115b54393440fbc4c70e11f6c86f4689b8
Travis McDemus <travis@mcdem.us>
2018-11-14 15:51:41 -0500

    Add solution for problem 905

--- 0905-sort-array-by-parity.go
+++ 0905-sort-array-by-parity.go
@@ -1,7 +1,15 @@
 package leetcode
 
 func sortArrayByParity(A []int) []int {
-	return []int{}
+	evens, odds := make([]int, 0), make([]int, 0)
+	for _, a := range A {
+		if a%2 == 0 {
+			evens = append(evens, a)
+		} else {
+			odds = append(odds, a)
+		}
+	}
+	return append(evens, odds...)
 }
 
 /*
