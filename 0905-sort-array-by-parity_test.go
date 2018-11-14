92d8828978c5947142e718e88a1485aaac08b0cd
Travis McDemus <travis@mcdem.us>
2018-11-14 15:49:12 -0500

    Add test case for problem 905

--- 0905-sort-array-by-parity_test.go
+++ 0905-sort-array-by-parity_test.go
@@ -1,7 +1,21 @@
 package leetcode
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func SortArrayByParity(t *testing.T) {
+	testCases := []struct {
+		in  []int
+		out []int
+	}{
 
+		{[]int{3, 1, 2, 4}, []int{2, 4, 3, 1}},
+	}
+	for _, tc := range testCases {
+		if out := sortArrayByParity(tc.in); !reflect.DeepEqual(out, tc.out) {
+			t.Errorf("Expected %v but received %v for input %v", tc.out, out, tc.in)
+		}
+	}
 }
