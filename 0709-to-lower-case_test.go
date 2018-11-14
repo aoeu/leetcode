092f54c749a9f249f653b8450d93af9ce558fef8
Travis McDemus <travis@mcdem.us>
2018-11-14 14:21:07 -0500

    Add test cases for problem 709

--- 0709-to-lower-case_test.go
+++ 0709-to-lower-case_test.go
@@ -3,5 +3,17 @@ package leetcode
 import "testing"
 
 func TestToLowerCase(t *testing.T) {
-
+	testCases := []struct {
+		in  string
+		out string
+	}{
+		{"Hello", "hello"},
+		{"here", "here"},
+		{"LOVELY", "lovely"},
+	}
+	for _, tc := range testCases {
+		if s := toLowerCase(tc.in); s != tc.out {
+			t.Errorf("Expected '%v' but received '%v' for input '%v'", tc.out, s, tc.in)
+		}
+	}
 }
