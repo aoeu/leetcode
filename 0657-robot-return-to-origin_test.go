dbfaf239974406caefc30babe2845d14afe2d4a4
Travis McDemus <travis@mcdem.us>
2018-11-14 16:03:23 -0500

    Add test cases for problem 657

--- 0657-robot-return-to-origin_test.go
+++ 0657-robot-return-to-origin_test.go
@@ -3,5 +3,16 @@ package leetcode
 import "testing"
 
 func TestJudgeCircle(t *testing.T) {
-
+	testCases := []struct {
+		in  string
+		out bool
+	}{
+		{"UD", true},
+		{"LL", false},
+	}
+	for _, tc := range testCases {
+		if out := judgeCircle(tc.in); out != tc.out {
+			t.Errorf("Expected %v but received %v for input '%v'", tc.out, out, tc.in)
+		}
+	}
 }
